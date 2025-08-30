35cf586bfca739cbb1083010d74278fdba3a90b7
Zein0 <ahmadalzein55@outlook.com>
2025-08-30 23:43:41 +0300

    fixed timezone issue

--- src/services/booking.js
+++ src/services/booking.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 const { addDays, setHours, setMinutes, setSeconds, setMilliseconds } = require('date-fns');
-const { zonedTimeToUtc, utcToZonedTime } = require('date-fns-tz');
+const { toZonedTime } = require('date-fns-tz');
 
 const prisma = new PrismaClient();
 
@@ -248,12 +248,12 @@ async function getAvailability(doctorId, date) {
     
     // Fix timezone issue: convert UTC to Beirut time for hour comparison
     const globalCount = appointments.filter(apt => {
-      const beirutTime = utcToZonedTime(apt.startAt, 'Asia/Beirut');
+      const beirutTime = toZonedTime(apt.startAt, 'Asia/Beirut');
       return beirutTime.getHours() === hour;
     }).length;
     
     const doctorBusy = doctorId && appointments.some(apt => {
-      const beirutTime = utcToZonedTime(apt.startAt, 'Asia/Beirut');
+      const beirutTime = toZonedTime(apt.startAt, 'Asia/Beirut');
       return apt.doctorId === doctorId && beirutTime.getHours() === hour;
     });
 
